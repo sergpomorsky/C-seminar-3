fc9a640148f0d323952be922da39679557bb66b9
deardaddy <sergpomorsky@gmail.com>
2022-06-26 12:31:16 +0300

    Add task 23

---
+++ task_23_homework/Program.cs
@@ -0,0 +1,12 @@
+﻿// *Задача 23: * Напишите программу, которая принимает 
+//на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = 1;
+int[]array = new int [A];
+while(B <= A)
+{
+array[B-1] = B*B*B;
+Console.WriteLine(array[B-1]);
+B++;
+}
